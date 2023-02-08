2518f35e0d2d0fa570e3ab1d4f0c00166b0b089d
Johan Du Toit <ghostwolf1512@gmail.com>
2023-02-08 22:22:34 +0200

    V0: Vanilla Files

---
+++ @VPPAdminTools/meta.cpp
@@ -0,0 +1,4 @@
+protocol = 1;
+publishedid = 1828439124;
+name = "VPPAdminTools";
+timestamp = 5249740006185415254;
---
+++ STARTValning.bat
@@ -0,0 +1,11 @@
+@echo off
+:start
+::Launch parameters (edit end: -config=|-port=|-profiles=|-doLogs|-adminLog|-netlog|-freezeCheck|-filePatching|-BEpath=|-cpuCount=)
+start DayZServer_x64.exe -config=serverDZ.cfg -port=2302 -profiles=Profilesfolder "-mod=@Dabs Framework;@VPPAdminTools;@CF" -cpuCount=%serverCPU% -dologs -adminlog -netlog -freezecheck
+::Time in seconds before kill server process (14400 = 4 hours)
+timeout 3600
+taskkill /im DayZServer_x64.exe /F
+::Time in seconds to wait before..
+timeout 10
+::Go back to the top and repeat the whole cycle again
+goto start 
